c023eb2b6f2a553f775644c8153314ef13a73a2a
yorickdevries <yorickdevries@live.com>
2019-09-14 18:47:14 +0200

    change tab to 4 space

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         try {
-        	//String instance = args[0];
-        	String instance = "../instances/n_2_k_10_dmax_100_0.txt";
+            //String instance = args[0];
+            String instance = "../instances/n_2_k_10_dmax_100_0.txt";
             System.out.println(new BruteForceSolver().solve(AuctionProblemInstance.IO.read(instance)).value); // replace with your solver here
         } catch (IOException e) {
             e.printStackTrace();
